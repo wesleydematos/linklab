0f0a7b84a258e954d3a06a2f2a7db1677795e662
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-12-14 11:16:30 -0300

    feat: updated Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,26 @@
+"use client"
+
+import { useState } from 'react'
+import Image from 'next/image'
+import logo from '../../../public/halogenn.png'
+import menu from '../../../public/menu.png'
+import budgets from '../../../public/budgets.png'
+import styles from '@/styles/header.module.sass'
+import HeaderModal from '@/components/HeaderModal'
+
 export default function Header() {
-    return (
-      <header>
-        header
+  const [modal, setModal] = useState(false)
+
+  return (
+    <>
+      <header className={styles.header}>
+        <Image src={logo} width={144} height={31} alt='Logo halogenn'/>
+        <div className={styles.mobile_items}>
+          <Image src={budgets} width={41} height={41} alt='Budgets'/>
+          <Image src={menu} width={29} height={29} alt='Menu hamburguer' onClick={()=> setModal(!modal)}/> 
+        </div>
       </header>
-    )
-  }
+      {modal && <HeaderModal/>}
+    </>
+  )
+}
