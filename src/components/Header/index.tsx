fd95541cea914db2018564b2eb824597bef572ea
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-12-14 17:02:12 -0300

    feat: added desktop nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,6 +7,7 @@ import styles from '@/styles/header.module.sass'
 import logo from '../../../public/halogenn.png'
 import menu from '../../../public/menu.png'
 import budgets from '../../../public/budgets.png'
+import bud from '../../../public/bud.png'
 
 export default function Header() {
   const [modal, setModal] = useState(false)
@@ -14,11 +15,25 @@ export default function Header() {
   return (
     <>
       <header className={styles.header}>
-        <Image src={logo} width={144} height={31} alt='Logo halogenn' priority/>
+        <Image src={logo} width={163} height={42} alt='Logo halogenn' priority/>
         <div className={styles.mobile_items}>
           <Image src={budgets} width={41} height={41} alt='Budgets'/>
           <Image src={menu} width={29} height={29} alt='Menu hamburguer' onClick={()=> setModal(!modal)}/> 
         </div>
+        <nav>
+          <div>
+            <a href='#'>Início</a>
+            <a href='#'>Produtos <span> v</span></a>
+            <a href='#'>Laudos</a>
+            <a href='#'>Sobre Nós</a>
+            <a href='#'>Certificações</a>
+            <a href='#'>Contato</a>
+          </div>
+          <a href="#">
+            <Image width={15} height={15} src={bud} alt='budget'/>
+            Orçamento
+          </a>
+        </nav>
       </header>
       {modal && <HeaderModal/>}
     </>
