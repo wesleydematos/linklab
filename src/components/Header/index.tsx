41171b4ea04b0116ce390eaf358f66246ea2bfa1
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-12-14 15:36:05 -0300

    chore: updated imports for better organization

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import { useState } from 'react'
 import Image from 'next/image'
+import { useState } from 'react'
+import { HeaderModal } from '@/components'
+import styles from '@/styles/header.module.sass'
 import logo from '../../../public/halogenn.png'
 import menu from '../../../public/menu.png'
 import budgets from '../../../public/budgets.png'
-import styles from '@/styles/header.module.sass'
-import HeaderModal from '@/components/HeaderModal'
 
 export default function Header() {
   const [modal, setModal] = useState(false)
@@ -14,7 +14,7 @@ export default function Header() {
   return (
     <>
       <header className={styles.header}>
-        <Image src={logo} width={144} height={31} alt='Logo halogenn'/>
+        <Image src={logo} width={144} height={31} alt='Logo halogenn' priority/>
         <div className={styles.mobile_items}>
           <Image src={budgets} width={41} height={41} alt='Budgets'/>
           <Image src={menu} width={29} height={29} alt='Menu hamburguer' onClick={()=> setModal(!modal)}/> 
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,9 @@
 import Header from "./Header";
 import MobileHeader from "./MobileHeader";
+import HeaderModal from "./HeaderModal";
 
 export {
   Header,
-  MobileHeader
+  MobileHeader,
+  HeaderModal
 }
