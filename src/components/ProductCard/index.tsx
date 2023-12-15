1095f7a632320e5ac6911280136e20059af12ace
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-12-15 17:28:25 -0300

    feat: created product card

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,7 +1,26 @@
-export default function ProductCard () {
+"use client"
+
+import Image from "next/image"
+
+interface iProduct {
+  _id: string
+  name: string
+  image: string
+  value: number
+  note: number
+}
+
+export default function ProductCard ({ product }: { product: iProduct }) {
   return (
-    <div>
-      product card
-    </div>
+    <li>
+      <Image src={product.image} width={300} height={150} alt={product.name}/>
+      <div>
+        <div>
+          <p>{product.name}</p>
+          <span>Nota: {product.note % 1 === 0 ? `${product.note}.0` : product.note}</span>
+        </div>
+        <p>R${product.value},00</p>
+      </div>
+    </li>
   )
 }
